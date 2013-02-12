3fdc192ea508e9c72296d174a3de5ef0c047aa43
Steve Winton <stevewinton@gmail.com>
2013-02-12 19:17:04 +0000

    Added South to project setup

--- src/emmasocial/settings.py
+++ src/emmasocial/settings.py
@@ -150,6 +150,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
     'bootstrapform',
     'uni_form',
     'allauth',
